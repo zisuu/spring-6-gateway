cc757df2f7bc9a805ce6b8ad7d22ca2ffca61027
zisuu <david.ziswiler@finecloud.ch>
2023-05-01 22:16:46 +0200

    first commit

---
+++ src/test/java/ch/finecloud/spring6gateway/Spring6GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.finecloud.spring6gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
